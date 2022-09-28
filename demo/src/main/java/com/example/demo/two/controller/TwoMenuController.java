3038e1538e88394b09a42b2c8b28a0f011cffef6
chunwoong park <chunwoong0104@gmail.com>
2022-09-28 10:38:02 +0900

    Menu2 commit

---
+++ demo/src/main/java/com/example/demo/two/controller/TwoMenuController.java
@@ -0,0 +1,20 @@
+package com.example.demo.two.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/two")
+public class TwoMenuController {
+	
+	@GetMapping("/menu")
+	public String twoMenuPage(Model model) {
+		
+		model.addAttribute("message" , "Menu Two 개발 완료!");
+		
+		return "two/result";
+	}
+	
+}
